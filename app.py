d42cc98d7d7d0a1d0aee30917edce60b6c0f402c
sarrahab <101526730+sarrahab@users.noreply.github.com>
2024-04-22 12:50:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,8 @@ app.layout = html.Div(children=[
     
     html.P('''
         Welcome to our HR Analytics Dashboard! This powerful tool brings you closer to understanding the intricacies of employee engagement, performance, and satisfaction across the spectrum of departments, job positions, and diverse demographic backgrounds. 
-        Leverage our intuitive filters to segment the data meaningfully, and embark on your journey towards data-driven HR excellence today. Discover trends, pinpoint opportunities for improvement, and make informed decisions that lead to impactful outcomes.
+        Leverage the intuitive filters to segment the data meaningfully, and embark on your journey towards data-driven HR excellence today. Discover trends, pinpoint opportunities for improvement, and make informed decisions that lead to impactful outcomes.
+        This data is from 10 years ago, but was updated 3 years ago to include more variables.
         ''', style={'textAlign': 'center', 'color': 'white', 'backgroundColor': '#4b2e83', 'padding': '20px'}), # white text color for the description of the app, purple background
     
         html.Div([
